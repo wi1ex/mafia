fbbc80005cd62f138272c65b2eb799cb78ebb190
wi1ex <aleksanyan.artem@gmail.com>
2025-11-10 20:33:38 +0300

    7 create game

--- frontend/src/components/RoomSetting.vue
+++ frontend/src/components/RoomSetting.vue
@@ -10,53 +10,55 @@
 
       <div class="change-devices">
         <div class="switch-div">
-          <div class="switch-text">Шумо- и эхоподавление:</div>
-          <label class="switch">
-            <input type="checkbox" :checked="nsOn" @change="onToggleNs" aria-label="Шумоподавление" />
-            <div class="slider">
-              <div class="slider-text" aria-hidden="true">Откл</div>
-              <div class="slider-text" aria-hidden="true">Вкл</div>
-            </div>
-          </label>
+          <div class="switch">
+            <span>Шумо- и эхоподавление:</span>
+            <label>
+              <input type="checkbox" :checked="nsOn" @change="onToggleNs" aria-label="Шумоподавление" />
+              <div class="slider">
+                <span>Откл</span>
+                <span>Вкл</span>
+              </div>
+            </label>
+          </div>
+
+          <div class="switch">
+            <span>Отражение камеры:</span>
+            <label>
+              <input type="checkbox" :checked="mirrorOn" @change="onToggleMirror" aria-label="Зеркальность" />
+              <div class="slider">
+                <span>Откл</span>
+                <span>Вкл</span>
+              </div>
+            </label>
+          </div>
+
+          <div class="switch">
+            <span>Качество видео:</span>
+            <label>
+              <input type="checkbox" :checked="vq === 'hd'" :disabled="vqDisabled" @change="onToggleVQ" aria-label="Качество видео: SD/HD" />
+              <div class="slider">
+                <span>SD</span>
+                <span>HD</span>
+              </div>
+            </label>
+          </div>
         </div>
 
-        <div class="switch-div">
-          <div class="switch-text">Отражение камеры:</div>
-          <label class="switch">
-            <input type="checkbox" :checked="mirrorOn" @change="onToggleMirror" aria-label="Зеркальность" />
-            <div class="slider">
-              <div class="slider-text" aria-hidden="true">Откл</div>
-              <div class="slider-text" aria-hidden="true">Вкл</div>
-            </div>
-          </label>
-        </div>
-
-        <div class="switch-div">
-          <div class="switch-text">Качество видео:</div>
-          <label class="switch">
-            <input type="checkbox" :checked="vq === 'hd'" :disabled="vqDisabled" @change="onToggleVQ" aria-label="Качество видео: SD/HD" />
-            <div class="slider">
-              <div class="slider-text" aria-hidden="true">SD</div>
-              <div class="slider-text" aria-hidden="true">HD</div>
-            </div>
-          </label>
-        </div>
-
-        <div class="switch-title-device">
-          <div class="switch-title">Выбор камеры:</div>
+        <div class="switch-device-div">
+          <span>Выбор камеры:</span>
           <div class="switch-device" role="listbox" aria-label="Список камер">
             <div class="switch-device-item" v-for="d in cams" :key="d.deviceId" role="option" :aria-selected="d.deviceId === camId" @click="pickDevice('videoinput', d.deviceId)" >
-              <div class="switch-device-item-text">{{ d.label || 'Камера' }}</div>
+              <span>{{ d.label || 'Камера' }}</span>
               <img :src="iconCheck" alt="" v-if="d.deviceId === camId" />
             </div>
           </div>
         </div>
 
-        <div class="switch-title-device">
-          <div class="switch-title">Выбор микрофона:</div>
+        <div class="switch-device-div">
+          <span>Выбор микрофона:</span>
           <div class="switch-device" role="listbox" aria-label="Список микрофонов">
             <div class="switch-device-item" v-for="d in mics" :key="d.deviceId" role="option" :aria-selected="d.deviceId === micId" @click="pickDevice('audioinput', d.deviceId)" >
-              <div class="switch-device-item-text">{{ d.label || 'Микрофон' }}</div>
+              <span>{{ d.label || 'Микрофон' }}</span>
               <img :src="iconCheck" alt="" v-if="d.deviceId === micId" />
             </div>
           </div>
@@ -126,7 +128,6 @@ function pickDevice(kind: 'audioinput'|'videoinput', id: string) {
   flex-direction: column;
   right: 0;
   bottom: 50px;
-  gap: 10px;
   width: 400px;
   border-radius: 5px;
   background-color: $dark;
@@ -162,73 +163,68 @@ function pickDevice(kind: 'audioinput'|'videoinput', id: string) {
     display: flex;
     flex-direction: column;
     align-items: flex-start;
-    padding: 17px;
-    gap: 17px;
+    padding: 10px;
+    gap: 10px;
     background-color: $dark;
     .switch-div {
       display: flex;
-      align-items: center;
-      justify-content: space-between;
+      flex-direction: column;
+      gap: 5px;
       width: 100%;
-      .switch-text {
-        font-size: 16px;
-        color: $fg;
-        line-height: 100%;
-      }
       .switch {
-        position: relative;
-        display: inline-block;
-        width: 140px;
-        height: 30px;
-        input {
-          position: absolute;
-          opacity: 0;
-          width: 0;
-          height: 0;
-        }
-        .slider {
-          display: flex;
-          align-items: center;
-          justify-content: space-around;
-          position: absolute;
-          inset: 0;
-          cursor: pointer;
-          background-color: $graphite;
-          border-radius: 5px;
-          .slider-text {
-            font-size: 14px;
-            color: $grey;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        label {
+          position: relative;
+          width: 140px;
+          height: 30px;
+          input {
+            position: absolute;
+            opacity: 0;
+            width: 0;
+            height: 0;
           }
-        }
-        .slider:before {
-          content: "";
-          position: absolute;
-          top: 3px;
-          left: 3px;
-          width: 64px;
-          height: 24px;
-          background-color: $fg;
-          border-radius: 4px;
-          transition: transform 0.25s ease-in-out;
-        }
-        input:checked + .slider:before {
-          transform: translateX(70px);
-        }
-        input:disabled + .slider {
-          opacity: 0.5;
-          cursor: not-allowed;
+          .slider {
+            display: flex;
+            align-items: center;
+            justify-content: space-around;
+            position: absolute;
+            inset: 0;
+            cursor: pointer;
+            background-color: $graphite;
+            border-radius: 5px;
+            span {
+              font-size: 14px;
+              color: $grey;
+            }
+          }
+          .slider:before {
+            content: "";
+            position: absolute;
+            top: 3px;
+            left: 3px;
+            width: 64px;
+            height: 24px;
+            background-color: $fg;
+            border-radius: 5px;
+            transition: transform 0.25s ease-in-out;
+          }
+          input:checked + .slider:before {
+            transform: translateX(70px);
+          }
+          input:not(:checked) + .slider span:first-child,
+          input:checked + .slider span:last-child {
+            color: $fg;
+          }
         }
       }
     }
-    .switch-title-device {
+    .switch-device-div {
       display: flex;
       flex-direction: column;
       width: 100%;
-      gap: 10px;
-      .switch-title {
-        font-size: 16px;
-        color: $fg;
-      }
+      gap: 5px;
       .switch-device {
         width: 100%;
         max-height: 140px;
@@ -244,11 +240,8 @@ function pickDevice(kind: 'audioinput'|'videoinput', id: string) {
           height: 34px;
           cursor: pointer;
           background: none;
-          &:hover {
-            background-color: $graphite;
-          }
-          .switch-device-item-text {
-            font-size: 14px;
+          span {
+            font-size: 12px;
             color: $fg;
             white-space: nowrap;
             overflow: hidden;
