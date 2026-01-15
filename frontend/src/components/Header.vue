457cc5795c30677584d699fb885f442fcc12f0e8
wi1ex <aleksanyan.artem@gmail.com>
2026-01-15 22:10:26 +0300

    20 some fix

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -2,8 +2,7 @@
   <header class="bar">
     <div class="links">
       <router-link class="btn" :to="{ name: 'home' }" aria-label="DECEIT.games">
-<!--        <span>deceit.games {{ BUILD }}</span>-->
-        <span>deceit.games v0.8.4b</span>
+        <span>deceit.games {{ BUILD }}</span>
       </router-link>
       <router-link v-if="user.user?.role === 'admin'" class="btn" :to="{ name: 'admin' }" aria-label="Админ-панель">
         <span>Админ-панель</span>
