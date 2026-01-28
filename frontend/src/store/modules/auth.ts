05abf27e13ba86c6bc2453ca61a7c5de38f2db8f
wi1ex <aleksanyan.artem@gmail.com>
2026-01-28 19:06:31 +0300

    22 some fix

--- frontend/src/store/modules/auth.ts
+++ frontend/src/store/modules/auth.ts
@@ -142,8 +142,6 @@ export const useAuthStore = defineStore('auth', () => {
       delLS([
         'auth:sid',
         'auth:lock',
-        'ui:hide_install',
-        'ui:hotkeys',
         'room:lastRoom',
         'room:lastGame'
       ])
