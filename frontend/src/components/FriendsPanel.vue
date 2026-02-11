c0c18f201c9de86bacb0de65a71ba4ac10e0d446
wi1ex <aleksanyan.artem@gmail.com>
2026-02-11 03:02:05 +0300

    23 add friends

--- frontend/src/components/FriendsPanel.vue
+++ frontend/src/components/FriendsPanel.vue
@@ -461,8 +461,11 @@ onBeforeUnmount(() => {
           border-radius: 50%;
         }
         .nick {
-          height: 16px;
-          font-size: 14px;
+          height: 18px;
+          font-size: 16px;
+          white-space: nowrap;
+          overflow: hidden;
+          text-overflow: ellipsis;
         }
       }
       .info {
